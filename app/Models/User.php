110c6e6d9a95118c600c13435471b83502933af8
mim16296 <rehana@softvence.com>
2026-06-13 14:52:57 +0600

    updated route

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,11 +31,6 @@ class User extends Authenticatable
 
 
 
-    //profile
-    public function profile()
-{
-    return $this->hasOne(Profile::class);
-}
 
     /**
      * The attributes that should be hidden for serialization.
