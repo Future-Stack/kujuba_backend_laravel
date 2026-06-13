339f5eab5e771c875b89d594ec55996853ceeba4
sabbir-softvence <sabbirhossain@softvence.com>
2026-06-13 11:32:53 +0600

    added base url inside api.php

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,13 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('v1')->group(function () {
+    Route::get('/', function () {
+        return response()->json([
+            'message' => 'Welcome to API v1',
+            'status' => 'ok',
+            'version' => '1.0'
+        ]);
+    });
 
     // ----------------------------
     // Public Routes
