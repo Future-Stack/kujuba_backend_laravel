1e175963e2241f7e58dbacd4624bb44a2669f988
Md. Mahbubur Rahman <mahbubur@softvence.com>
2026-06-13 14:02:25 +0600

    authentication usdate with profile

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\InspectionBookingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,9 +25,17 @@ Route::prefix('v1')->group(function () {
 
         Route::post('logout', [AuthController::class, 'logout']);
 
-    });
+        Route::get('/user-profile', [AuthController::class, 'getProfile']);
 
-});
+        Route::post('/profile/update', [AuthController::class, 'updateProfile']);
+
+        Route::post('/book-inspection', [InspectionBookingController::class, 'store']);
+
+        Route::get('/my-inspections', [InspectionBookingController::class, 'index']);
+
+        });
+
+    });
     // Route::get('/user', function (Request $request) {
     //     return $request->user();
     // })->middleware('auth:sanctum');
