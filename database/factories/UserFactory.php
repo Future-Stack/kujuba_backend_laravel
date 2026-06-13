7c1913e13250ff571ecd07729ac9a730300d50b6
sabbir-softvence <sabbirhossain@softvence.com>
2026-06-13 09:43:30 +0600

    modified faker class

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,9 +6,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
+
 class UserFactory extends Factory
 {
     /**
@@ -24,8 +22,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' =>$this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
--- deployment.md
+++ deployment.md
@@ -3,5 +3,8 @@
 1) Create cd.yaml in .github/workflow
 2) Create VPS_PASSWORD,VPS_HOST,DOCKER_USERNAME,DOCKER_PASSWORD secrets in 
 Settings → Secrets and variables → Actions → Repository secret
-3) 
+3) added docker-compose.yaml, docker-entrypoint,Dockerfile in local project.
+4) create .env,.env.docker, app & docker-compose.yml in vps folder.
+5) push code & run github workflow.
+6) 
 
