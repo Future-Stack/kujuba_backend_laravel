3cdd46eb374d35a67ae97477050ec6ebbbf34392
sabbir-softvence <sabbirhossain@softvence.com>
2026-06-22 10:27:38 +0600

    added stripe account id in inspector seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -75,7 +75,8 @@ class UserSeeder extends Seeder
                 'license_number' => 'LIC-12345',
                 'license_expiry' => now()->addYear(),
                 'insurance_expiry' => now()->addYear(),
-                'stripe_onboarding_completed' => 1
+                'stripe_onboarding_completed' => 1,
+                'stripe_account_id' =>'acct_1TkglDFlBbC7u2Vb'
             ]
         );
 
