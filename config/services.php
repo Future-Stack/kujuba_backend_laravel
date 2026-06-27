274627e81b5aba27d01cd47eb0388ebce7e8f119
sabbir-softvence <sabbirhossain@softvence.com>
2026-06-27 10:21:42 +0600

    added service stripe

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'stripe' => [
         'secret' => env('STRIPE_SECRET'),
         'publishable_key' => env('STRIPE_PUBLIC_KEY'),
-        'payout_webhook_secret'  => env('Payout_WEBHOOK_SECRET'),
+        'payout_webhook_secret'  => env('PAYOUT_WEBHOOK_SECRET'),
         'booking_webhook_secret' => env('WEBHOOK_SECRET'),
         'cancel_webhook_secret' => env('CANCEL_WEBHOOK_SECRET'),
     ],
