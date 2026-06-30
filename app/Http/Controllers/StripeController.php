b153df0a08177a89c316fd43b2a69d27e114830e
Md. Mahbubur Rahman <mahbubur@softvence.com>
2026-06-30 07:29:15 +0600

    onbording issue

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -227,9 +227,8 @@ class StripeController extends Controller
             $isComplete       = $detailsSubmitted && $payoutsEnabled;
 
             // DB sync 
-            $user->profile->update([
-                'stripe_onboarding_completed' => $isComplete ? 1 : 0,
-            ]);
+            Profile::where('user_id', $userId)
+                ->update(['stripe_onboarding_completed' => $isComplete ? 1 : 0]);
 
             return response()->json([
                 'success'                     => true,
